e9c9cd535379d89c17f029630347b7c976465012
Hoda Rajaei <rajaei.hoda@gmail.com>
2020-11-04 10:53:16 -0500

    add run_script

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -t 7-00:00:00
 #SBATCH -n 4
-#SBATCH --gres=gpu:tesla-v100:2
+#SBATCH --gres=gpu:tesla-v100:4
 #SBATCH -p gablab
 #SBATCH --mem=32G
 #SBATCH -o /om2/user/hodaraja/kwyk/nobrainer_scripts/outputs/slurm_2gpu_%j.out
@@ -17,6 +17,6 @@ singularity exec \
 -B /om/user/satra/kwyk/tfrecords/ \
 -B /om2/user/hodaraja \
 "$IMAGE" \
-python3 run_kwyk_mirror_trainbatch.py 128
+python3 run_kwyk_mirror_trainbatch.py 128 concrete
 
 echo 'Finished'
