3f2f88546272ea3f8caa5f15243cb63d418d97c8
Hoda Rajaei <rajaei.hoda@gmail.com>
2020-11-18 13:14:04 -0500

    corrected env activation

--- satori_run_script.sh
+++ satori_run_script.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-#SBATCH -t 7-00:00:00
+#SBATCH -t 12:00:00
+#SBATCH -N 1
 #SBATCH -n 4
 #SBATCH --gres=gpu:4
-#SBATCH --mem=32G
-#SBATCH -o kwyk_train.out
+#SBATCH --mem=64G
+#SBATCH -o outputs/kwyk_train_%j.out
+#SBATCH --exclusive
 
-set -e
-source ~/.bashrc
-conda activate nobrainer
 
+## User python environment
+HOME2=/nobackup/users/$(whoami)
+PYTHON_VIRTUAL_ENVIRONMENT=nobrainer
+CONDA_ROOT=$HOME2/miniconda3
+
+## Activate environment
+source ${CONDA_ROOT}/etc/profile.d/conda.sh
+conda activate $PYTHON_VIRTUAL_ENVIRONMENT
+ulimit -s unlimited
+
+## Command to run
 python3 run_kwyk_mirror_trainbatch.py 128 concrete
 
 echo 'Finished'
