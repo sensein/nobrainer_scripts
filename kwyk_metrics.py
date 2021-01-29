c1144eb6e4a8a00e28884dce5e125c36b588564e
Hoda Rajaei <rajaei.hoda@gmail.com>
2021-01-29 15:03:03 -0800

    added kwyk_metrics

---
+++ kwyk_metrics.py
@@ -0,0 +1,29 @@
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("seaborn-whitegrid")
+
+root_path="training_files/"
+model_name="kwyk_check_21-01-26_19-06"
+file_name="data-kwyk_check_21-01-26_19-06.json"
+
+def plot_vars(dic):
+    """ plot items in dic"""
+    fig, ax = plt.subplots(1,len(dic), figsize=[11, 3])
+    for i, key in enumerate(dic.keys()):
+        data = dic[key]
+        ax[i].plot(np.linspace(0,len(data),len(data)),data)
+        ax[i].set_title(key)
+        ax[i].set_xlabel("batch number")
+        
+    
+path=os.path.join(root_path,model_name,file_name)
+
+with open(path) as f:
+    data=json.load(f)
+
+plot_vars(data)
+plt.show()
+
+
