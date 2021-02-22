f0849a06fa1ea8d437f8e4c7b5763d2c296de7af
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2021-02-22 13:43:34 -0500

    Update kwyk_losses.py

--- kwyk_losses.py
+++ kwyk_losses.py
@@ -69,3 +69,46 @@ class ELBO(LossFunctionWrapper):
             reduction=reduction,
         )
 
+def kwyk_loss(y_true, y_pred, model ,num_examples, warm_start, from_logits=False):
+    means = model.layers.kernel_posterior.mean()
+    sigmas = model.layers.kernel_posterior.std()
+    mean_priors = model.layers.priors.mean()
+    sigma_priors = model.layers.priors.sigma() 
+    neg_log_likelihood = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_pred, logits=y_true))
+    l2_reg_loss = tf.reduce_sum((tf.square(means - mean_priors))/ ((tf.square(sigma_priors) + 1e-8) * 2.0))
+    sigma_sq_loss = tf.reduce_sum(tf.square(sigmas) / ((tf.square(sigma_priors) + 1e-8) * 2.0))
+    log_sigma_loss = tf.reduce_sum(tf.log(sigmas+1e-8))
+    
+    kld = sum(model.losses)
+    if warm_start:
+        loss = neg_log_likelihood + (l2_reg_loss)/num_examples
+    else:
+        loss = neg_log_likelihood + (l2_reg_loss + sigma_sq_loss - log_sigma_loss + kld)/ num_examples
+    return loss
+
+
+class KWYK_Loss(LossFunctionWrapper):
+    """Loss to minimize Evidence Lower Bound (ELBO).
+
+    Use this loss for multiclass variational segmentation.
+    Labels should not be one-hot encoded.
+    """
+
+    def __init__(
+        self,
+        model,
+        num_examples,
+        from_logits=False,
+        reduction=tf.keras.losses.Reduction.NONE,
+        name="ELBO",
+        warm_start = False
+    ):
+        super().__init__(
+            kwyk_loss,
+            model=model,
+            num_examples=num_examples,
+            from_logits=from_logits,
+            name=name,
+            reduction=tf.keras.losses.Reduction.NONE,
+            warm_start = warm_start
+        )
